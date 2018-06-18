491a0d095133d1443e6b4a7dd1d97c99e5518d8c
AllanJeremy <aj.dev254@gmail.com>
2018-06-18 23:51:56 +0300

    Minor comit

--- singularity/lib/main.dart
+++ singularity/lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
 
-
+import 'screens/user/match.dart';
 
 //TODO: Check if the user has been set up and show appropriate home page 
-
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text('My application')
-      )
-    );
-  }
-}
+void main() => runApp(new MatchPage());
